a760d456bb585aa796ae059d45bec94e35b92876
Victoria Meng <nights1ststar@protonmail.com>
2018-06-05 20:50:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,17 +27,16 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @artist = Song.artist
     erb :'songs/edit'
   end
 
   post '/songs/:slug/edit' do
-    binding.pry
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     @artist = Artist.find_or_create_by(name: params[:artist_name])
     @song.update(artist: @artist)
     flash[:message] = "Successfully updated song."
-    binding.pry
+    # binding.pry
     redirect "/songs/#{@song.slug}/edit"
   end
 
