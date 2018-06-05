5f7c605ea7463e45b27e0de26cc61074fb5128c4
Victoria Meng <nights1ststar@protonmail.com>
2018-06-05 21:44:18 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,7 +34,6 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     binding.pry
     @artist = Artist.find_or_create_by(params[:song][:artist][:name])
-    @genres =
     flash[:message] = "Successfully updated song."
     binding.pry
     redirect "/songs/#{@song.slug}"
